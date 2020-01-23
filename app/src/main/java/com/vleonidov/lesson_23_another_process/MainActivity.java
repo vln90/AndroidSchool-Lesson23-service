881efb63bf0b6ad8b3a41b506694960228feb8dd
Леонидов Василий Николаевич <Leonidov.V.Ni@sberbank.ru>
2020-01-23 21:13:59 +0300

    Add messenger IPC.

---
+++ app/src/main/java/com/vleonidov/lesson_23_another_process/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vleonidov.lesson_23_another_process;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
